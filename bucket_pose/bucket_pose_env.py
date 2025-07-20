224a9abea814991ed3d717855e94b473e03ca743
ryota ishizu <ryota6621@gmail.com>
2025-07-20 19:25:24 +0900

    バケット姿勢制御
    https://github.com/ishizur/pai_proj/issues/10

---
+++ bucket_pose/bucket_pose_env.py
@@ -0,0 +1,436 @@
+import math
+from itertools import chain
+
+import genesis as gs
+import torch
+from genesis.utils.geom import (
+    inv_quat,
+    quat_to_xyz,
+    transform_by_quat,
+)
+
+
+def gs_rand_float(lower: float, upper: float, shape, device):
+    return (upper - lower) * torch.rand(size=shape, device=device) + lower
+
+
+def cartesian_to_spherical(cartesian_coords: torch.Tensor) -> torch.Tensor:
+    """
+    3次元直交座標系 (x, y, z) から極座標（球座標系） (r, theta, phi) に変換します。
+
+    Args:
+        cartesian_coords (torch.Tensor): (n, 3) の形状を持つTensor。各行が (x, y, z) 座標を表します。
+
+    Returns:
+        torch.Tensor: (n, 3) の形状を持つTensor。各行が (r, theta, phi) 座標を表します。
+                      - r: 原点からの距離（半径）
+                      - theta: z軸正方向からの角度（極角）。範囲は [0, pi] です。
+                      - phi: xy平面上でのx軸正方向からの角度（方位角）。範囲は [-pi, pi] です。
+    """
+    x = cartesian_coords[:, 0]
+    y = cartesian_coords[:, 1]
+    z = cartesian_coords[:, 2]
+
+    # ゼロ除算を避けるための微小な値
+    eps = 1e-8
+
+    # 半径 r の計算
+    r = torch.norm(cartesian_coords, p=2, dim=1)
+
+    # 極角 theta の計算
+    # 浮動小数点精度の問題で acos の引数が [-1, 1] の範囲外になるのを防ぐために clamp を使用
+    theta = torch.acos(torch.clamp(z / (r + eps), -1.0, 1.0))
+
+    # 方位角 phi の計算
+    phi = torch.atan2(y, x)
+
+    # 結果を (r, theta, phi) の順でスタックして返す
+    spherical_coords = torch.stack((r, theta, phi), dim=1)
+
+    return spherical_coords
+
+
+def spherical_diff(p1: torch.Tensor, p2: torch.Tensor):
+    """
+    p1, p2: [n, 3] tensor 各行が (r, theta, phi)
+    returns: (r, theta, phi) tensors of shape [n]
+    """
+
+    # 半径差（符号付き）
+    delta_r = p2[:, 0] - p1[:, 0]
+
+    # 角度差（正規化して -π〜π に収める）
+    raw_theta_diff = p2[:, 1] - p1[:, 1]
+    raw_phi_diff = p2[:, 2] - p1[:, 2]
+    delta_theta = torch.remainder(raw_theta_diff + math.pi, 2 * math.pi) - math.pi
+    delta_phi = torch.remainder(raw_phi_diff + math.pi, 2 * math.pi) - math.pi
+    normalized_diff = torch.stack((delta_r, delta_theta, delta_phi), dim=1)
+
+    return normalized_diff
+
+
+class BucketPoseEnv:
+    def __init__(
+        self, num_envs, env_cfg, obs_cfg, reward_cfg, command_cfg, show_viewer=False
+    ):
+        self.num_envs = num_envs
+        self.num_obs = obs_cfg["num_obs"]
+        self.num_privileged_obs = None
+        self.num_actions = env_cfg["num_actions"]
+        self.num_commands = command_cfg["num_commands"]
+        self.device = gs.device
+
+        self.simulate_action_latency = env_cfg["simulate_action_latency"]
+        self.dt = 0.01  # control frequency on real robot is 100hz
+        self.max_episode_length = math.ceil(env_cfg["episode_length_s"] / self.dt)
+
+        self.env_cfg = env_cfg
+        self.obs_cfg = obs_cfg
+        self.reward_cfg = reward_cfg
+        self.command_cfg = command_cfg
+
+        self.obs_scales = obs_cfg["obs_scales"]
+        self.reward_scales = reward_cfg["reward_scales"]
+
+        # create scene
+        self.scene = gs.Scene(
+            sim_options=gs.options.SimOptions(dt=self.dt, substeps=2),
+            viewer_options=gs.options.ViewerOptions(
+                max_FPS=env_cfg["max_visualize_FPS"],
+                camera_pos=(10.0, 10.0, 10.0),
+                camera_lookat=(0.0, 0.0, 1.0),
+                camera_fov=40,
+            ),
+            vis_options=gs.options.VisOptions(rendered_envs_idx=list(range(1))),
+            rigid_options=gs.options.RigidOptions(
+                dt=self.dt,
+                constraint_solver=gs.constraint_solver.Newton,
+                enable_collision=True,
+                enable_joint_limit=True,
+            ),
+            show_viewer=show_viewer,
+        )
+        # add plane
+        self.scene.add_entity(gs.morphs.Plane())
+
+        # add target
+        if self.env_cfg["visualize_target"]:
+            self.target = self.scene.add_entity(
+                morph=gs.morphs.Mesh(
+                    file="meshes/sphere.obj",
+                    scale=0.05,
+                    fixed=False,
+                    collision=False,
+                ),
+                surface=gs.surfaces.Rough(
+                    diffuse_texture=gs.textures.ColorTexture(
+                        color=(1.0, 0.5, 0.5),
+                    ),
+                ),
+            )
+        else:
+            self.target = None
+
+        # add camera
+        if self.env_cfg["visualize_camera"]:
+            self.cam = self.scene.add_camera(
+                res=(640, 480),
+                pos=(10.0, 10.0, 10.0),
+                lookat=(0.0, 0.0, 1.0),
+                fov=40,
+                GUI=False,
+            )
+
+        # add robot
+        self.base_init_pos = torch.tensor(
+            self.env_cfg["base_init_pos"], device=gs.device
+        )
+        self.base_init_quat = torch.tensor(
+            self.env_cfg["base_init_quat"], device=gs.device
+        )
+        self.inv_base_init_quat = inv_quat(self.base_init_quat)
+        self.robot = self.scene.add_entity(
+            gs.morphs.URDF(file="../assets/zx120/zx120.urdf"),
+        )
+
+        # build
+        self.scene.build(n_envs=num_envs)
+
+        # names to indices
+        self.motors_dof_idx = list(
+            chain.from_iterable(
+                [
+                    self.robot.get_joint(name).dofs_idx_local
+                    for name in self.env_cfg["joint_names"]
+                ]
+            )
+        )
+        self.bucket_end = self.robot.get_link("bucket_end_link")
+
+        # prepare reward functions and multiply reward scales by dt
+        self.reward_functions, self.episode_sums = dict(), dict()
+        for name in self.reward_scales.keys():
+            self.reward_scales[name] *= self.dt
+            self.reward_functions[name] = getattr(self, "_reward_" + name)
+            self.episode_sums[name] = torch.zeros(
+                (self.num_envs,), device=gs.device, dtype=gs.tc_float
+            )
+
+        # initialize buffers
+        self.base_pos = torch.zeros(
+            (self.num_envs, 3), device=gs.device, dtype=gs.tc_float
+        )
+        self.base_quat = torch.zeros(
+            (self.num_envs, 4), device=gs.device, dtype=gs.tc_float
+        )
+        self.base_ang_vel = torch.zeros(
+            (self.num_envs, 3), device=gs.device, dtype=gs.tc_float
+        )
+        self.obs_buf = torch.zeros(
+            (self.num_envs, self.num_obs), device=gs.device, dtype=gs.tc_float
+        )
+        self.rew_buf = torch.zeros(
+            (self.num_envs,), device=gs.device, dtype=gs.tc_float
+        )
+        self.reset_buf = torch.ones((self.num_envs,), device=gs.device, dtype=gs.tc_int)
+        self.episode_length_buf = torch.zeros(
+            (self.num_envs,), device=gs.device, dtype=gs.tc_int
+        )
+        self.commands = torch.zeros(
+            (self.num_envs, self.num_commands), device=gs.device, dtype=gs.tc_float
+        )
+        self.commands_cart = torch.zeros(
+            (self.num_envs, self.num_commands - 1), device=gs.device, dtype=gs.tc_float
+        )
+        self.actions = torch.zeros(
+            (self.num_envs, self.num_actions), device=gs.device, dtype=gs.tc_float
+        )
+        self.last_actions = torch.zeros_like(self.actions)
+        self.dof_pos = torch.zeros_like(self.actions)
+        self.dof_vel = torch.zeros_like(self.actions)
+        self.bucket_end_pos = torch.zeros(
+            (self.num_envs, 3), device=gs.device, dtype=gs.tc_float
+        )
+        self.bucket_pose = torch.zeros(
+            (self.num_envs,), device=gs.device, dtype=gs.tc_float
+        )
+        self.last_bucket_end_pos = torch.zeros_like(self.bucket_end_pos)
+        self.default_dof_pos = torch.tensor(
+            [
+                self.env_cfg["default_joint_angles"][name]
+                for name in self.env_cfg["joint_names"]
+            ],
+            device=gs.device,
+            dtype=gs.tc_float,
+        )
+        self.extras = dict()  # extra information for logging
+        self.extras["observations"] = dict()
+
+    def _resample_commands(self, envs_idx):
+        r = gs_rand_float(*self.command_cfg["r_range"], (len(envs_idx),), gs.device)
+        az = gs_rand_float(
+            *self.command_cfg["azimuth_range"], (len(envs_idx),), gs.device
+        )
+        alt = gs_rand_float(
+            *self.command_cfg["altitude_range"], (len(envs_idx),), gs.device
+        )
+        bucket_pitch = gs_rand_float(
+            *self.command_cfg["bucket_pitch_range"], (len(envs_idx),), gs.device
+        )
+        self.commands[envs_idx, 0] = r
+        self.commands[envs_idx, 1] = alt
+        self.commands[envs_idx, 2] = az
+        self.commands[envs_idx, 3] = bucket_pitch
+        self.commands_cart[envs_idx, 0] = r * torch.cos(alt) * torch.cos(az)
+        self.commands_cart[envs_idx, 1] = r * torch.cos(alt) * torch.sin(az)
+        self.commands_cart[envs_idx, 2] = r * torch.sin(alt)
+
+    def _at_target(self):
+        self.at_target = (
+            (
+                (
+                    torch.norm(self.rel_pos_cart, dim=1)
+                    < self.env_cfg["at_target_threshold"]
+                )
+                & (
+                    torch.abs(self.bucket_pose - self.commands[:, 3])
+                    < self.env_cfg["bucket_pose_threshold"]
+                )
+            )
+            .nonzero(as_tuple=False)
+            .flatten()
+        )
+        return self.at_target
+
+    def step(self, actions):
+        self.actions = torch.clip(
+            actions, -self.env_cfg["clip_actions"], self.env_cfg["clip_actions"]
+        )
+        exec_actions = (
+            self.last_actions if self.simulate_action_latency else self.actions
+        )
+        target_dof_vel = exec_actions * self.env_cfg["action_scale"]
+        self.robot.control_dofs_velocity(target_dof_vel, self.motors_dof_idx)
+        # update target pos
+        if self.target is not None:
+            self.target.set_pos(self.commands_cart, zero_velocity=True)
+        self.scene.step()
+
+        # update buffers
+        self.episode_length_buf += 1
+        self.base_pos[:] = self.robot.get_pos()
+        self.base_quat[:] = self.robot.get_quat()
+        inv_base_quat = inv_quat(self.base_quat)
+        self.base_ang_vel[:] = transform_by_quat(self.robot.get_ang(), inv_base_quat)
+        self.last_bucket_end_pos[:] = self.bucket_end_pos[:]
+        bckt_end_pos = self.bucket_end.get_pos()
+        self.rel_pos_cart = self.commands_cart - bckt_end_pos
+        self.bucket_end_pos[:] = cartesian_to_spherical(bckt_end_pos)
+
+        self.rel_pos = spherical_diff(self.commands[:, :3], self.bucket_end_pos)
+        self.last_rel_pos = spherical_diff(
+            self.commands[:, :3], self.last_bucket_end_pos
+        )
+
+        self.dof_pos[:] = self.robot.get_dofs_position(self.motors_dof_idx)
+        self.dof_vel[:] = self.robot.get_dofs_velocity(self.motors_dof_idx)
+        self.bucket_pose[:] = torch.sum(self.dof_pos[:, 1:], dim=1) + 1.57079632679
+
+        # resample commands
+        envs_idx = self._at_target()
+        self._resample_commands(envs_idx)
+
+        # check termination and reset
+        self.reset_buf = self.episode_length_buf > self.max_episode_length
+
+        time_out_idx = (
+            (self.episode_length_buf > self.max_episode_length)
+            .nonzero(as_tuple=False)
+            .flatten()
+        )
+        self.extras["time_outs"] = torch.zeros_like(
+            self.reset_buf, device=gs.device, dtype=gs.tc_float
+        )
+        self.extras["time_outs"][time_out_idx] = 1.0
+
+        self.reset_idx(self.reset_buf.nonzero(as_tuple=False).flatten())
+
+        # compute reward
+        self.rew_buf[:] = 0.0
+        for name, reward_func in self.reward_functions.items():
+            rew = reward_func() * self.reward_scales[name]
+            self.rew_buf += rew
+            self.episode_sums[name] += rew
+
+        # compute observations
+        self.obs_buf = torch.cat(
+            [
+                self.rel_pos * self.obs_scales["rel_pos"],  # 3
+                self.bucket_pose.reshape(-1, 1),  # 1
+                self.commands,  # 4
+                self.dof_pos * self.obs_scales["dof_pos"],  # 4
+                self.dof_vel * self.obs_scales["dof_vel"],  # 4
+                self.actions,  # 4
+            ],
+            axis=-1,
+        )
+
+        self.last_actions[:] = self.actions[:]
+
+        self.extras["observations"]["critic"] = self.obs_buf
+
+        return self.obs_buf, self.rew_buf, self.reset_buf, self.extras
+
+    def get_observations(self):
+        self.extras["observations"]["critic"] = self.obs_buf
+        return self.obs_buf, self.extras
+
+    def get_privileged_observations(self):
+        return None
+
+    def reset_idx(self, envs_idx):
+        if len(envs_idx) == 0:
+            return
+
+        # reset dofs
+        self.dof_pos[envs_idx] = self.default_dof_pos
+        self.dof_vel[envs_idx] = 0.0
+        self.robot.set_dofs_position(
+            position=self.dof_pos[envs_idx],
+            dofs_idx_local=self.motors_dof_idx,
+            zero_velocity=True,
+            envs_idx=envs_idx,
+        )
+
+        # reset base
+        self.base_pos[envs_idx] = self.base_init_pos
+        self.base_quat[envs_idx] = self.base_init_quat.reshape(1, -1)
+        self.robot.set_pos(
+            self.base_pos[envs_idx], zero_velocity=False, envs_idx=envs_idx
+        )
+        self.robot.set_quat(
+            self.base_quat[envs_idx], zero_velocity=False, envs_idx=envs_idx
+        )
+        self.base_ang_vel[envs_idx] = 0
+        self.robot.zero_all_dofs_velocity(envs_idx)
+
+        # reset buffers
+        self.bucket_end_pos[envs_idx] = cartesian_to_spherical(
+            self.bucket_end.get_pos(envs_idx)
+        )
+        self.last_bucket_end_pos[envs_idx] = self.bucket_end_pos[envs_idx]
+        self.bucket_pose[envs_idx] = (
+            torch.sum(self.dof_pos[envs_idx, 1:], dim=1) + 1.57079632679
+        )
+
+        self.rel_pos = spherical_diff(self.commands[:, :3], self.bucket_end_pos)
+        self.last_rel_pos = spherical_diff(
+            self.commands[:, :3], self.last_bucket_end_pos
+        )
+        self.last_actions[envs_idx] = 0.0
+        self.episode_length_buf[envs_idx] = 0
+        self.reset_buf[envs_idx] = True
+
+        # fill extras
+        self.extras["episode"] = {}
+        for key in self.episode_sums.keys():
+            self.extras["episode"]["rew_" + key] = (
+                torch.mean(self.episode_sums[key][envs_idx]).item()
+                / self.env_cfg["episode_length_s"]
+            )
+            self.episode_sums[key][envs_idx] = 0.0
+
+        self._resample_commands(envs_idx)
+
+    def reset(self):
+        self.reset_buf[:] = True
+        self.reset_idx(torch.arange(self.num_envs, device=gs.device))
+        return self.obs_buf, None
+
+    # ------------ reward functions----------------
+    def _reward_target(self):
+        last_sq = torch.abs(self.last_rel_pos)
+        now_sq = torch.abs(self.rel_pos)
+        target_rew = (last_sq[:, 0] - now_sq[:, 0]) + 10.0 * (
+            torch.sum(last_sq[:, 1:], dim=1) - torch.sum(now_sq[:, 1:], dim=1)
+        )
+        return target_rew
+
+    def _reward_bucket_pose(self):
+        pose_rew = torch.abs(self.bucket_pose - self.commands[:, 3])
+        return pose_rew
+
+    def _reward_smooth(self):
+        smooth_rew = torch.sum(
+            torch.abs(torch.sign(self.actions) - torch.sign(self.last_actions)), dim=1
+        )  # signs of the velocity input consistency
+        return smooth_rew
+
+    def _reward_target_arrival(self):
+        arrival_rew = torch.zeros((self.num_envs,), device=gs.device, dtype=gs.tc_float)
+        arrival_rew[self.at_target] = 1
+        return arrival_rew
+
+    def _reward_angular(self):
+        angular_rew = torch.norm(self.base_ang_vel, dim=1)
+        return angular_rew
---
+++ bucket_pose/train_pose.py
@@ -0,0 +1,166 @@
+import argparse
+import os
+import pickle
+import shutil
+from importlib import metadata
+
+try:
+    try:
+        if metadata.version("rsl-rl"):
+            raise ImportError
+    except metadata.PackageNotFoundError:
+        if metadata.version("rsl-rl-lib") != "2.2.4":
+            raise ImportError
+except (metadata.PackageNotFoundError, ImportError) as e:
+    raise ImportError(
+        "Please uninstall 'rsl_rl' and install 'rsl-rl-lib==2.2.4'."
+    ) from e
+from math import pi
+
+import genesis as gs
+from bucket_pose_env import BucketPoseEnv
+from rsl_rl.runners import OnPolicyRunner
+
+
+def get_train_cfg(exp_name, max_iterations):
+    train_cfg_dict = {
+        "algorithm": {
+            "class_name": "PPO",
+            "clip_param": 0.2,
+            "desired_kl": 0.01,
+            "entropy_coef": 0.01,
+            "gamma": 0.99,
+            "lam": 0.95,
+            "learning_rate": 0.001,
+            "max_grad_norm": 1.0,
+            "num_learning_epochs": 5,
+            "num_mini_batches": 4,
+            "schedule": "adaptive",
+            "use_clipped_value_loss": True,
+            "value_loss_coef": 1.0,
+        },
+        "init_member_classes": {},
+        "policy": {
+            "activation": "elu",
+            "actor_hidden_dims": [512, 256, 128],
+            "critic_hidden_dims": [512, 256, 128],
+            "init_noise_std": 1.0,
+            "class_name": "ActorCritic",
+        },
+        "runner": {
+            "checkpoint": -1,
+            "experiment_name": exp_name,
+            "load_run": -1,
+            "log_interval": 1,
+            "max_iterations": max_iterations,
+            "record_interval": -1,
+            "resume": False,
+            "resume_path": None,
+            "run_name": "",
+        },
+        "runner_class_name": "OnPolicyRunner",
+        "num_steps_per_env": 24,
+        "save_interval": 100,
+        "empirical_normalization": None,
+        "seed": 1,
+    }
+
+    return train_cfg_dict
+
+
+def get_cfgs():
+    env_cfg = {
+        "num_actions": 4,
+        # joint/link names
+        "default_joint_angles": {  # [rad]
+            "swing_joint": 0.0,
+            "boom_joint": -0.5,
+            "arm_joint": 1.6,
+            "bucket_joint": 0.96,
+        },
+        "joint_names": [
+            "swing_joint",
+            "boom_joint",
+            "arm_joint",
+            "bucket_joint",
+        ],
+        # base pose
+        "base_init_pos": [0.0, 0.0, 0.0],
+        "base_init_quat": [1.0, 0.0, 0.0, 0.0],
+        "episode_length_s": 20.0,
+        "at_target_threshold": 0.5,
+        "bucket_pose_threshold": 0.1,  # about 5~6 degs
+        "action_scale": 1.0,
+        "simulate_action_latency": False,
+        "clip_actions": 1.0,
+        "visualize_target": False,
+        "visualize_camera": False,
+        "max_visualize_FPS": 60,
+    }
+    obs_cfg = {
+        "num_obs": 20,
+        "obs_scales": {
+            "rel_pos": 1.0,
+            "dof_pos": 1.0,
+            "dof_vel": 1.0,
+        },
+    }
+    reward_cfg = {
+        "reward_scales": {
+            "target": 10.0,
+            "target_arrival": 1000.0,
+            "smooth": -0.1,
+            "angular": -1,
+            "bucket_pose": -0.1,
+        },
+    }
+    command_cfg = {
+        "num_commands": 4,
+        "r_range": [3.5, 6.0],
+        "azimuth_range": [-pi, pi],
+        "altitude_range": [pi / 10, pi / 4],
+        "bucket_pitch_range": [pi, 1.5 * pi],
+    }
+
+    return env_cfg, obs_cfg, reward_cfg, command_cfg
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="bucket-pose")
+    parser.add_argument("-B", "--num_envs", type=int, default=4096)
+    parser.add_argument("--max_iterations", type=int, default=1001)
+    args = parser.parse_args()
+
+    gs.init(logging_level="warning")
+
+    log_dir = f"logs/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg = get_cfgs()
+    train_cfg = get_train_cfg(args.exp_name, args.max_iterations)
+
+    if os.path.exists(log_dir):
+        shutil.rmtree(log_dir)
+    os.makedirs(log_dir, exist_ok=True)
+
+    pickle.dump(
+        [env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg],
+        open(f"{log_dir}/cfgs.pkl", "wb"),
+    )
+
+    env = BucketPoseEnv(
+        num_envs=args.num_envs,
+        env_cfg=env_cfg,
+        obs_cfg=obs_cfg,
+        reward_cfg=reward_cfg,
+        command_cfg=command_cfg,
+    )
+
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device=gs.device)
+
+    runner.learn(
+        num_learning_iterations=args.max_iterations, init_at_random_ep_len=True
+    )
+
+
+if __name__ == "__main__":
+    main()
